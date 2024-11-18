4c651765da0ccbdecd1077bd2e1d86f142142fef
Pigolo58 <sarayut.mas@wipay.com>
2024-11-18 16:30:21 +0700

    Add protocol ESP NOW

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,74 @@
 // #include <demos/lv_demos.h>
 #include <ui.h>
 #include <HardwareSerial.h>
+#include <esp_now.h>
+#include <WiFi.h>
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = { 0x90,0x38,0x0c,0x63,0xef,0x58};
 
+typedef struct test_swich {
+  bool swich_1;
+  bool swich_2;
+  bool swich_3;
+} test_swich;
+test_swich swichdata;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void send_data_swich_ON(bool s1,bool s2,bool s3);
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+void send_data_swich_ON(bool s1,bool s2,bool s3){
+  swichdata.swich_1 = s1;
+  swichdata.swich_2 = s2;
+  swichdata.swich_3 = s3;
+  esp_err_t result = esp_now_send(0, (uint8_t *) &swichdata, sizeof(swichdata));
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
+void On_relay220VAC(lv_event_t * e)
+{
+	// Your code here
+}
+
+void Btb_close(lv_event_t * e)
+{
+	// Your code here
+   send_data_swich_ON(0,0,0);
+
+}
+
+void Pressd_S1(lv_event_t * e)
+{
+	// Your code here
+  send_data_swich_ON(1,0,0);
+}
+
+void Pressd_S3(lv_event_t * e)
+{
+	// Your code here
+  send_data_swich_ON(0,0,1);
+}
+
+void Pressd_S2(lv_event_t * e)
+{
+	// Your code here
+  send_data_swich_ON(0,1,0);
+}
 void setup()
 {
   delay(200);
@@ -14,6 +81,22 @@ void setup()
   //lv_demo_benchmark();
   //lv_demo_music();
   ui_init();
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop()
